fcd587df72db97318a2c6ff3bdcc2fb9885fb784
John Tan <john.v.tan@gmail.com>
2017-07-11 23:39:05 -0400

    adding old files

--- testing.py
+++ testing.py
@@ -14,11 +14,17 @@ def main():
     game_board = board.Board()
     test_box = board.Box()
     test_box.init_rect(100, 500)
+    screen.fill((255, 255, 255))
+    
+    game_board.update(1, 'med', (255, 0, 0), (1, 1))
+    game_board.update(2, 'big', (0, 255, 0), (0, 0))
+    game_board.update(3, 'small', (0, 0, 255), (2, 1))
+    game_board._print_board()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: running = False
    
-        screen.fill((250, 250, 250)) 
+        #screen.fill((255, 255, 255)) 
         #test_box.draw_box(screen)
         game_board.draw_board(screen)
         pygame.display.flip()
