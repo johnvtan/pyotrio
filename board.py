45433af5bde475017095f720bdd931157a4d4c58
John Tan <john.v.tan@gmail.com>
2017-07-11 23:24:37 -0400

    Found old otrio code - Board class works, not sure about otrio or player yet

--- board.py
+++ board.py
@@ -14,15 +14,15 @@ class Box():
         # also need to track colors
         self.big = False
         self.big_color = None
-        self.big_rad = 200
+        self.big_rad = 80 
 
         self.med = False
         self.med_color = None
-        self.med_rad = 100
+        self.med_rad = 50
        
         self.small = False
         self.small_color = None
-        self.small_rad = 50        
+        self.small_rad = 25       
 
         # next two are going to be used later
         # rect is a pygame.rect object
@@ -35,12 +35,26 @@ class Box():
         #self.area = (None, None, None, None)
         # constants relevant to the box
         self.side_length = 200
-        self.line_width = 2 
+        self.line_width = 10 
 
     def init_rect(self, left, top):
         self.rect = pygame.Rect(left, top, self.side_length, self.side_length)
         #self.area = (left, top, self.side_length, self.side_length)
-        self.center = (left + self.side_length / 2, top - self.side_length / 2)
+        self.center = (int(left + self.side_length / 2), int(top + self.side_length / 2))
+
+    def add_piece(self, size, color):
+        ''' adds a piece to the box (big, medium, small) '''
+        if size == 'big':
+            self.big = True
+            self.big_color = color
+        elif size == 'med':
+            self.med = True
+            self.med_color = color
+        elif size == 'small':
+            self.small = True
+            self.small_color = color
+        else:
+            print('Error: invalid piece size')
 
     def draw_box(self, surface):
         ''' function for drawing everything inside the box '''
@@ -58,9 +72,22 @@ class Board():
     def __init__(self):
         
         # defines board_state as 3x3 array of Box() objects
+        # note that board_state is entirely for visual purposes
         self.board_state =[[Box(), Box(), Box()], [Box(), Box(), Box()], [Box(), Box(), Box()]]
         
-        # coords defines board_state[0][0] as the bottom left and[2][2] as top right
+        # raw_state is what's used to check if win, and keep track of which 
+        # player played in which boxes. Each atomic (?) list represents a single box
+        # where the order is (Big, Med, Small)
+        self.raw_state =  [[[0, 0, 0] for i in range(3)] for i in range(3)]
+
+        # last move and last player are to make checking the win condition easy
+        # last_move has the coords of the last move played
+        self.last_move = None
+
+        # last_player has the number of the player who played the last move
+        self.last_player = None
+        
+        # coords defines board_state[0][0] as the top left and [2][2] as bottom right
         self.coords = [(left * 200 + OFFSET[0], top * 200 + OFFSET[1]) for left in range(3) for top in range(3)]
         
         # setting the coordinates of each box and initializing each rectangle
@@ -73,15 +100,32 @@ class Board():
                 box.init_rect(self.coords[count][0],self.coords[count][1])
                 count += 1
 
-    def update_board(self, player, piece, location):
-        # we need the player object (which should have the color/number??), the piece placed, and the location (ideally, in coordinates)
-        pass
+    def update(self, player, size, color, location):
+        # for now, lets say player is simply a number (1-4), size is a string
+        # (big, med, small), and location is a tuple (x, y)
+        
+        # update the raw state
+        self.raw_state[location[0]][location[1]] = player
+        
+        # now update the board state
+        self.board_state[location[0]][location[1]].add_piece(size, color)
+       
 
     def check_if_win(self):
+        # returns True if last_player won, False otherwise
         pass
+               
     
     def draw_board(self, surface):
         for row in self.board_state:
             for box in row:
                 box.draw_box(surface)
 
+    def get_board(self):
+        return self.raw_state
+
+    def _print_board(self):
+        ''' helper function to print out raw board state '''
+        for i in range(3):
+            for j in range(3):
+                print("Row: %d, Col: %d, player: %d" % (i, j, self.raw_state[i][j]))
